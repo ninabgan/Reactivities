ff9560f2ef38ed19f75adeee597ac86011a96542
ninabgan <ninabgan@hotmail.no>
2024-02-26 13:22:19 +0100

    end of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -13,6 +13,7 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -25,6 +26,7 @@ export default function ActivityDashboard({
   closeForm,
   createOrEdit,
   deleteActivity,
+  submitting,
 }: Props) {
   return (
     <Grid>
@@ -33,6 +35,7 @@ export default function ActivityDashboard({
           activities={activities}
           selectActivity={selectActivity}
           deleteActivity={deleteActivity}
+          submitting={submitting}
         />
       </Grid.Column>
       <Grid.Column width="6">
@@ -44,6 +47,7 @@ export default function ActivityDashboard({
           closeForm={closeForm}
           activity={selectedActivity}
           createOrEdit={createOrEdit}
+          submitting={submitting}
         />
       </Grid.Column>
     </Grid>
